894f74dede53f20e848fb1d909eaa2dfedb6f97a
Carson <jackparadise520a@gmail.com>
2025-08-26 14:26:02 +0800

    change: - and +

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+const VERSION = "0.0.0"
+
+func init() {
+
+}
+
 func main() {
 	fmt.Println("Test git diff")
 
 	fmt.Println("Hello world")
 
-	fmt.Println("0123456789")
+	fmt.Println(VERSION)
 }
